0f23b06e0df075ac28981a9ffb43c950bcde0540
Hanish2022 <itz.hanish@gmail.com>
2024-10-09 20:00:40 +0530

    redux store added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-console.log(import.meta.env.VITE_APPWRITE_URL);
+// console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
      <h1>blog app che</h1>
